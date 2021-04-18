08010d6d0b43178f00a7575515a97da0e7416a72
LumberJacksolotl <82751639+LumberJacksolotl@users.noreply.github.com>
2021-04-18 11:31:40 -0600

    New axolotls and line breaks

--- js/names.js
+++ js/names.js
@@ -2,4 +2,8 @@ export let names = {
     relaxolotl:'loves to relax',
     lumberjacksolotl:'Cuts down trees',
     maxolotl:"takes things TO THE MAX!!!!!",
+    Attacksolotl:"Attacks people",
+    Askolotl:"asks questions",
+    Hacksolotl:"hacker",
+    Saxolotl:"plays the saxophone"
 }
--- js/sort.js
+++ js/sort.js
@@ -3,19 +3,18 @@ import { names } from './names.js'
 let SortedNamesArray = Object.keys(names).sort()
 const displayDiv = document.getElementById('List')
 
-function display(n) {
-    const br = document.createElement("br");
+function display(index) {
 
-    let currentName = document.createTextNode(SortedNamesArray[n])
-    let currentDescription = document.createTextNode(names[SortedNamesArray[n]])
+    let currentName = document.createTextNode(SortedNamesArray[index] + ':')
+    let currentDescription = document.createTextNode(names[SortedNamesArray[index]])
 
     displayDiv.appendChild(currentName)
-    displayDiv.appendChild(br)
+    displayDiv.appendChild(document.createElement("br"))
     displayDiv.appendChild(currentDescription)
 }
 
 for (let i = 0; i < SortedNamesArray.length; i++) {
     display(i)
     displayDiv.appendChild(document.createElement("br"))
+    displayDiv.appendChild(document.createElement("br"))
 }
-console.log(displayDiv.innerHTML)
