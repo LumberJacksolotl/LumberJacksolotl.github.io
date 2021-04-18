249a2258cc89da8127271af5de9e5196a7c1e581
LumberJacksolotl <82751639+LumberJacksolotl@users.noreply.github.com>
2021-04-18 02:14:09 -0600

    Create sort.js

---
+++ js/sort.js
@@ -0,0 +1,21 @@
+import { names } from './names.js'
+
+let SortedNamesArray = Object.keys(names).sort()
+const displayDiv = document.getElementById('List')
+
+function display(n) {
+    const br = document.createElement("br");
+
+    let currentName = document.createTextNode(SortedNamesArray[n])
+    let currentDescription = document.createTextNode(names[SortedNamesArray[n]])
+
+    displayDiv.appendChild(currentName)
+    displayDiv.appendChild(br) //TODO: 
+    displayDiv.appendChild(currentDescription)
+}
+
+for (let i = 0; i < SortedNamesArray.length; i++) {
+    display(i)
+    displayDiv.appendChild(document.createElement("br"))
+}
+console.log(displayDiv.innerHTML)
